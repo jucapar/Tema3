3ca61fead3e1c5e2d3c11bf1525c3e42e0305440
jucapar <juanpastorcarlos@gmail.com>
2017-10-26 13:03:11 +0200

    Cambios 26/10/2017 3

--- Ejercicios/Ejercicio13.php
+++ Ejercicios/Ejercicio13.php
@@ -5,16 +5,25 @@
 	</head>
 		<title>Ejercicio 13</title>
 	<body>
-	
-		<?php
-			/*
-				Juan Carlos Pastor Regueras
-				Inicializar variables de los distintos tipos de datos básicos y mostrar los datos por pantalla. 
-				Fecha revision: 19-10-2017
-			*/
-			
-		?>
-		
+	<?php
+        //Autor: Juan Carlos Pastor
+		//Crear una función que cuente el número de visitas a la página actual desde una fecha concreta.
+        //Ultima modificación: 26/10/2017
+                $fecha_actual = strtotime(date("d-m-Y H:i:00",time()));
+                $fecha_requisito = strtotime("20-01-2017 00:00:00"); 
+                $archivo = "contador.txt";  
+                $fp = fopen($archivo,"r");//Abre el archivo contador.txt.
+                $contador = fgets($fp, 26); 
+                fclose($fp);
+                if($fecha_actual > $fecha_requisito){ 
+                    ++$contador;  
+                    $fp = fopen($archivo,"w+");  
+                    fwrite($fp, $contador, 26);  
+                    fclose($fp);                     
+                } 
+                 
+                echo "Numero de visitas $contador veces"; 
+            ?> 
 	</body>
 
 </html>
